0fb468448dd5b7786656d328ec1b77f750365a59
novaoneuu <dreamerofthewild1@gmail.com>
2026-05-11 23:33:57 +0100

    Update style.js

--- style.js
+++ style.js
@@ -1,14 +1,3 @@
-console.log("script running");
-
-window.addEventListener("mousemove", (e) => {
-  const glow = document.querySelector(".glow");
-  const glow2 = document.querySelector(".glow2");
-
-  if (!glow || !glow2) return;
-
-  const x = (e.clientX / window.innerWidth - 0.5) * 15;
-  const y = (e.clientY / window.innerHeight - 0.5) * 15;
-
-  glow.style.transform = `translate(${x}px, ${y}px)`;
-  glow2.style.transform = `translate(${-x}px, ${-y}px)`;
-});
+window.go = (path) => {
+  location.href = path;
+};
