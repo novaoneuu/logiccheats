166434a069ee9e29bc7d1200b0a4b68439969b74
novaoneuu <dreamerofthewild1@gmail.com>
2026-05-11 22:12:05 +0100

    Update style.js

--- style.js
+++ style.js
@@ -2,9 +2,15 @@ function go(path) {
   window.location.href = path;
 }
 
-window.addEventListener("DOMContentLoaded", () => {
+window.addEventListener("load", () => {
   const title = document.querySelector(".title");
   const buttons = document.querySelector(".buttons");
+
+  // force restart animation safely
+  title.classList.remove("play");
+  buttons.classList.remove("play");
+
+  void title.offsetWidth;
 
   title.classList.add("play");
   buttons.classList.add("play");
