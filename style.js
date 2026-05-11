24c53d2460904a8471c1fac0f41e7a52a72e5400
novaoneuu <dreamerofthewild1@gmail.com>
2026-05-11 21:50:36 +0100

    Update style.js

--- style.js
+++ style.js
@@ -1,1 +1,10 @@
+function go(path) {
+  window.location.href = path;
+}
 
+/* force reload animation replay */
+window.addEventListener("load", () => {
+  document.body.classList.remove("loaded");
+  void document.body.offsetWidth;
+  document.body.classList.add("loaded");
+});
