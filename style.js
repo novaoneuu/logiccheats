78d087d56504bb16d15429a34487129b95bfe529
novaoneuu <dreamerofthewild1@gmail.com>
2026-05-11 22:06:18 +0100

    Update style.js

--- style.js
+++ style.js
@@ -6,11 +6,6 @@ window.addEventListener("DOMContentLoaded", () => {
   const title = document.querySelector(".title");
   const buttons = document.querySelector(".buttons");
 
-  title.classList.remove("play");
-  buttons.classList.remove("play");
-
-  void title.offsetWidth;
-
   title.classList.add("play");
   buttons.classList.add("play");
 });
