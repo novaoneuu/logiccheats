ee9d515ad56bfcca245a0485882bd1d736b3f39a
novaoneuu <dreamerofthewild1@gmail.com>
2026-07-08 01:19:03 +0100

    Update style.js

--- style.js
+++ style.js
@@ -5,3 +5,21 @@ window.addEventListener("pageshow", () => {
   void card.offsetWidth;
   card.classList.add("animate");
 });
+
+
+function scrollProducts() {
+  document.querySelector("#products").scrollIntoView({
+    behavior: "smooth"
+  });
+}
+
+
+window.addEventListener("scroll", () => {
+  const hero = document.querySelector(".hero");
+
+  const blur = Math.min(window.scrollY / 40, 10);
+  const opacity = Math.max(1 - window.scrollY / 500, 0);
+
+  hero.style.filter = `blur(${blur}px)`;
+  hero.style.opacity = opacity;
+});
